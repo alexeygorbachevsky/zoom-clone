c94444aab9bb60baf9dddfff1d58141ec6972013
AlexeyGorbachevskiy <alexeygorbachevskiyy@gmail.com>
2023-07-07 13:21:19 +0800

    chore: configure vercel

--- backend/api/middlewares/errorHandler.ts
+++ backend/api/middlewares/errorHandler.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import createError from "http-errors";
 
 declare type WebError = Error & { status?: number };
-export const errorHandler = (err: WebError, req: Request, res: Response, next: NextFunction): void => {
+export const errorHandler = (err: WebError, req: Request, res: Response): void => {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get("env") === "development" ? err : {};
