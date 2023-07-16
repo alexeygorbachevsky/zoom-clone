de1b0faf0d82ee627c3476de389020f2d4637284
AlexeyGorbachevskiy <alexeygorbachevskiyy@gmail.com>
2023-07-16 13:37:46 +0800

    fix: debug backend vercel.json

--- frontend/src/api/ducks/constants.ts
+++ frontend/src/api/ducks/constants.ts
@@ -1,7 +1,6 @@
 import { ENV } from "constants/env";
 
 export const DOMAIN = {
-  // [ENV.development]: "https://localhost:5001",
-  [ENV.development]: "https://zoom-clone-backend-alexeygorbachevskiy.vercel.app",
+  [ENV.development]: "https://localhost:5001",
   [ENV.production]: "https://zoom-clone-backend-alexeygorbachevskiy.vercel.app",
 };
